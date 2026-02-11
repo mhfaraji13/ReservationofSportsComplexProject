d780acbb79c41edca2579a1e483ff6463d433c0b
mohammadhossein <mhfaraji13@gmail.com>
2026-02-11 15:04:47 +0330

    The method of forgetting the password and creating a new password by receiving an OTP has been implemented.

---
+++ ReservationSportsComplex.Infrastructure/Migrations/20260211112029_Add ForgotPassword.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReservationSportsComplex.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForgotPassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VerificationCode",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VerificationCodeExpiry",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VerificationCode",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationCodeExpiry",
+                table: "Users");
+        }
+    }
+}
